6288a72751e13a052b75e0521f5ab091c094091f
Héctor Vega <linnk99@gmail.com>
2020-10-02 16:54:14 -0500

    Add files via upload

---
+++ app/helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+if(! functon_exists('view')) {
+    function view($view) {
+        return new App\Http\Response();
+    }
+}
+
+if(! functon_exists('viewPath')) {
+    function viewPath($view) {
+        return __DIR__ . "/../views/$view.php";
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+
+require __DIR__ . '/../vendor/autolad.php';
+
+$request = new App\Http\Request;
+$request->send();
